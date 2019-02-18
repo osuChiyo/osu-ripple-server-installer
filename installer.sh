2a700274eec61f1b575b1f71921febd4e67fedae
Hazuki Onohara <minecraft172zzz@gmail.com>
2019-02-18 13:55:19 +0700

    Poor Redis :(

--- installer.sh
+++ installer.sh
@@ -292,11 +292,10 @@ read q
 if [ "$q" = "y" ]; then
 	apt-get install tmux -y
 	echo '#!/bin/sh' > tmux-start.sh
-	echo "tmux new-session -d -s redis 'tmux set remain-on-exit on && cd redis-stable/src && ./redis-server ../redis.conf'" >> tmux-start.sh
 	echo "tmux new-session -d -s avatar-server 'tmux set remain-on-exit on && cd avatar-server && ./avatar-server'" >> tmux-start.sh
 	echo "tmux new-session -d -s lets 'tmux set remain-on-exit on && cd lets && python3.6 lets.py'" >> tmux-start.sh
 	echo "tmux new-session -d -s bancho 'tmux set remain-on-exit on && cd pep.py && python3.6 pep.py'" >> tmux-start.sh
-	echo "tmux new-session -d -s rippleapi 'tmux set remain-on-exit on && cd rippleapi && ./rippleapi'" >> tmux-start.sh
+	echo "tmux new-session -d -s api 'tmux set remain-on-exit on && cd rippleapi && ./rippleapi'" >> tmux-start.sh
 	echo "tmux new-session -d -s hanayo 'tmux set remain-on-exit on && cd hanayo && ./hanayo'" >> tmux-start.sh
 	echo "echo TMUX window has been created. If they die restart them by calling ':respawn-window'" >> tmux-start.sh
 	chmod 777 tmux-start.sh
